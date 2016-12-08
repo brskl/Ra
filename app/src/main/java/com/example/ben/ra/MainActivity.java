31eddbae41d5a2bc87121ae55d16cc90b4ca9e8d
Benjamin <brskl@msn.com>
2016-12-07 20:25:46 -0800

    Added NewGame activity

--- app/src/main/java/com/example/ben/ra/MainActivity.java
+++ app/src/main/java/com/example/ben/ra/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ben.ra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMain(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.buttonPlay:
+            {
+                Log.v("UI", "Starting NewGameActivity");
+
+                Intent intent = new Intent(v.getContext(), NewGameActivity.class);
+                startActivity(intent);
+            }
+            break;
+            case R.id.buttonQuit:
+                Log.v("UI", "Exitting app");
+                finish();
+                break;
+        }
     }
 }

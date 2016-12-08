cee837aa8cc29e873b55d4b777283563841fbcd9
Benjamin <brskl@msn.com>
2016-12-07 22:33:09 -0800

    Added Create and onClickRadio code

--- app/src/main/java/com/example/ben/ra/NewGameActivity.java
+++ app/src/main/java/com/example/ben/ra/NewGameActivity.java
@@ -1,9 +1,14 @@
 package com.example.ben.ra;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class NewGameActivity extends AppCompatActivity {
     private int nPlayers;
@@ -21,6 +26,112 @@ public class NewGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_game);
 
         Log.d(NewGameActivity.class.toString(), "onCreate");
+
+        CheckBox cb;
+        EditText et;
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroupNumPlayers);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        String asNamesAiEasy[] = getResources().getStringArray(R.array.NamesAiEasy);
+        String asNamesAiMedium[] = getResources().getStringArray(R.array.NamesAiMedium);
+        String asNamesAiHard[] = getResources().getStringArray(R.array.NamesAiHard);
+
+        nPlayers = settings.getInt("NumPlayers", 3);
+        afHuman[0] = true;
+        afHuman[1] = settings.getBoolean("Player2Human", false);
+        afHuman[2] = settings.getBoolean("Player3Human", false);
+        afHuman[3] = settings.getBoolean("Player4Human", false);
+        afHuman[4] = settings.getBoolean("Player5Human", false);
+
+        for (int i = 0; i < Game.nMaxPlayers_c; i++)
+        {
+            if (afHuman[i])
+                asNames[i] = settings.getString(aisPrefNames[i], this.getResources().getString(R.string.defaultName));
+        }
+
+        aiAILevel[0] = 0;
+        aiAILevel[1] = 0;
+        aiAILevel[2] = 0;
+        aiAILevel[3] = 0;
+        aiAILevel[4] = 0;
+
+
+        switch(nPlayers)
+        {
+            default:
+                Log.w(NewGameActivity.class.toString(), "Preference for number of players was invalid");
+                nPlayers = 3;
+                // fall through
+            case 3:
+                rg.check(R.id.radioPlayers3);
+                break;
+            case 4:
+                rg.check(R.id.radioPlayers4);
+                break;
+            case 5:
+                rg.check(R.id.radioPlayers5);
+                break;
+        }
+
+        for (int i = 0; i < Game.nMaxPlayers_c; i++)
+        {
+            cb = (CheckBox) findViewById(aicbPlayers[i]);
+            cb.setChecked(afHuman[i]);
+            if (!afHuman[i])
+            {
+                asNames[i] = AIname(i);
+            }
+            et = (EditText) findViewById(aietPlayers[i]);
+            et.setText(asNames[i]);
+        }
+
+        EnableUx();
+    }
+
+    private String AIname(int iPlayer)
+    {
+        final String asNamesAiEasy[] = getResources().getStringArray(R.array.NamesAiEasy);
+        final String asNamesAiMedium[] = getResources().getStringArray(R.array.NamesAiMedium);
+        final String asNamesAiHard[] = getResources().getStringArray(R.array.NamesAiHard);
+
+        switch(aiAILevel[iPlayer])
+        {
+            default:
+                Log.w(NewGameActivity.class.toString(), "AI level was invalid for player " + (iPlayer+1));
+                aiAILevel[iPlayer] = 0;
+                // fall through
+            case 0:
+                return asNamesAiEasy[iPlayer];
+            case 1:
+                return asNamesAiMedium[iPlayer];
+            case 2:
+                return asNamesAiHard[iPlayer];
+        }
+    }
+
+    public void EnableUx()
+    {
+        View et;
+        CheckBox cb;
+
+        for (int i = 0; i < Game.nMaxPlayers_c; i++)
+        {
+            cb = (CheckBox) findViewById(aicbPlayers[i]);
+            et = findViewById(aietPlayers[i]);
+            et.setEnabled(cb.isChecked());
+            if (i >= 3)
+            {
+                if (i < nPlayers)
+                {
+                    cb.setVisibility(View.VISIBLE);
+                    et.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    cb.setVisibility(View.INVISIBLE);
+                    et.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
     }
 
     public void onClickNewGameRadio(View v)
@@ -28,7 +139,21 @@ public class NewGameActivity extends AppCompatActivity {
         Log.d(NewGameActivity.class.toString(), "onClickNewGameRadio");
 
         switch(v.getId()) {
+            case R.id.radioPlayers3:
+                Log.v(NewGameActivity.class.toString(), "radioPlayers3 pressed");
+                nPlayers = 3;
+                break;
+            case R.id.radioPlayers4:
+                Log.v(NewGameActivity.class.toString(), "radioPlayers4 pressed");
+                nPlayers = 4;
+                break;
+            case R.id.radioPlayers5:
+                Log.v(NewGameActivity.class.toString(), "radioPlayers5 pressed");
+                nPlayers = 5;
+                break;
         }
+
+        EnableUx();
     }
 
     public void onClickNewGameCheckbox(View v)

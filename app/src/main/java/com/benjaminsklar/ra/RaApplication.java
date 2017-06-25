a261b2c8dc781225e446a4724e1d6b61e7418a6c
brsklar <brskl@msn.com>
2017-06-24 21:13:20 -0700

    removed more AWS libs & imports

--- app/src/main/java/com/benjaminsklar/ra/RaApplication.java
+++ app/src/main/java/com/benjaminsklar/ra/RaApplication.java
@@ -3,10 +3,6 @@ package com.benjaminsklar.ra;
 import android.app.Application;
 import android.util.Log;
 
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-
 /**
  * Created by Ben on 5/29/2017.
  */

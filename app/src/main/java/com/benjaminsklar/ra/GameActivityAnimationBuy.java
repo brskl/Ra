0a9c2b1d4426e26761bb01cab5529ba7f6784b29
brsklar <brskl@msn.com>
2017-06-30 17:18:12 -0700

    in auction buy animation use pre-created ImageViews TODO: still need to have animation Listener work and imageViews be invisible after (even without alpha->0.0)

--- app/src/main/java/com/benjaminsklar/ra/GameActivityAnimationBuy.java
+++ app/src/main/java/com/benjaminsklar/ra/GameActivityAnimationBuy.java
@@ -2,6 +2,7 @@ package com.benjaminsklar.ra;
 
 import android.graphics.Rect;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 
 public class GameActivityAnimationBuy implements Animation.AnimationListener {
     GameActivity gameActivity;
+    ImageView aivAnimationAuctionTiles[];
 
     GameActivityAnimationBuy(GameActivity gameActivityValue)
     {
@@ -30,40 +32,33 @@ public class GameActivityAnimationBuy implements Animation.AnimationListener {
         Game game = Game.getInstance();
         int nTiles = game.getAuction().size();
         int i;
-        ImageView imageView;
         Rect rectStart = new Rect();
         Rect rectDest = new Rect();
 
+        aivAnimationAuctionTiles = new ImageView[nTiles];
         AnimationSet animationSet = new AnimationSet(true);
         gameActivity.arlPlayers[game.getAuctionPlayerHighestIndex()].getDrawingRect(rectDest);
         gameActivity.rlGameActivity.offsetDescendantRectToMyCoords(gameActivity.arlPlayers[game.getAuctionPlayerHighestIndex()], rectDest);
-        imageView = gameActivity.aivAuctionItems[0];
-        rectDest.offset(-imageView.getWidth() / 2, - imageView.getHeight()/2);
+        rectDest.offset(-gameActivity.aivAuctionItems[0].getWidth() / 2, - gameActivity.aivAuctionItems[0].getHeight()/2);
 
         for (i = 0; i < nTiles; i++) {
-            GameActivityAnimationDraw gameActivityAnimationTile = new GameActivityAnimationDraw(gameActivity);
             AnimationSet animationSetTile;
             TranslateAnimation translateAnimation;
             AlphaAnimation alphaAnimation;
             ImageView ivAuctionTile = gameActivity.aivAuctionItems[i];
-            // TODO: Create animation for particular tile and add to instance animationSet
+
+            aivAnimationAuctionTiles[i] = gameActivity.aivAnimationTiles[i];
 
             animationSetTile = new AnimationSet(true);
-            imageView = new ImageView(gameActivity);
-            imageView.setImageResource(gameActivity.TileImageRes(game.getAuction().get(i)));
+            aivAnimationAuctionTiles[i].setImageResource(gameActivity.TileImageRes(game.getAuction().get(i)));
 
             ivAuctionTile.getDrawingRect(rectStart);
             gameActivity.rlGameActivity.offsetDescendantRectToMyCoords(ivAuctionTile, rectStart);
 
-            ViewGroup.LayoutParams startLayout = ivAuctionTile.getLayoutParams();
-            ViewGroup.LayoutParams imageLayout = new ViewGroup.LayoutParams(startLayout.width, startLayout.height);
+            aivAnimationAuctionTiles[i].setX(rectStart.left - gameActivity.rlGameActivity.getLeft());
+            aivAnimationAuctionTiles[i].setY(rectStart.top - gameActivity.rlGameActivity.getTop());
 
-            imageView.setLayoutParams(imageLayout);
-            imageView.setX(rectStart.left - gameActivity.rlGameActivity.getLeft());
-            imageView.setY(rectStart.top - gameActivity.rlGameActivity.getTop());
-            gameActivity.rlGameActivity.addView(imageView);
-
-            imageView.setAnimation(animationSetTile);
+            aivAnimationAuctionTiles[i].setAnimation(animationSetTile);
 
             translateAnimation = new TranslateAnimation(0, rectDest.centerX() - rectStart.centerX(), 0, rectDest.centerY() - rectStart.centerY());
             translateAnimation.setDuration(1000);
@@ -75,17 +70,21 @@ public class GameActivityAnimationBuy implements Animation.AnimationListener {
             animationSetTile.addAnimation(translateAnimation);
             animationSetTile.addAnimation(alphaAnimation);
             animationSetTile.setFillAfter(true);
-            animationSetTile.setAnimationListener(gameActivityAnimationTile);
 
             animationSet.addAnimation(animationSetTile);
         }
         // TODO: Add swapping of Sun tiles
 
+        animationSet.setAnimationListener(this);
         return animationSet;
     }
 
     void close(Animation animation) {
         Log.d(GameActivityAnimationBuy.class.toString(), "close()");
+        for (ImageView imageView : aivAnimationAuctionTiles) {
+            imageView.setVisibility(View.INVISIBLE);
+        }
+        aivAnimationAuctionTiles = null;
     }
 
     public void onAnimationEnd(Animation animation) {
